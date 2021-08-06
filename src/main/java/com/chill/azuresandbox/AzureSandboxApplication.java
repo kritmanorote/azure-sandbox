af688ef9f911f36ce92789d129c451ea43bbdcdb
Kritakorn Kritmanorote <kkritmanorote@dstworldwideservices.com>
2021-08-07 00:27:28 +0700

    first commit

---
+++ src/main/java/com/chill/azuresandbox/AzureSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.chill.azuresandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chill/azuresandbox/AzureSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chill.azuresandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
